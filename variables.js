50c696f79a627b5fbfbb79b49a779ff2faab1862
William Lavery <lavery_william@yahoo.com>
2023-04-18 17:26:08 -0400

    add console.log and new strings

--- variables.js
+++ variables.js
@@ -1,4 +1,4 @@
-var name = "Dan Lavery";
+var me = "Dan Lavery";
 var greeting = "Hi there!";
 var sandwich = "Ham and Cheese";
 var date = 18;
@@ -7,3 +7,9 @@ var responses = 20;
 var present = true;
 var alive = true;
 var retired = false;
+me = "Sam Savery";
+greeting = "Hey There!";
+sandwich = "Spam and Cheese";
+console.log(me);
+console.log(greeting);
+console.log(sandwich);
